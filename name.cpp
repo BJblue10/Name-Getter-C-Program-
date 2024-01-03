de9ccb62755d29dafcef0463e5f36a922f32cb5a
Bettye <155570293+BJblue10@users.noreply.github.com>
2024-01-03 12:16:16 -0600

    Add files via upload

---
+++ name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string userName;
+   
+   cin >> userName;
+   cout << ""; 
+
+   cout << "Hey " <<userName << "!" << endl;
+   cout << "Welcome!" << endl;
+
+   return 0;
+}
